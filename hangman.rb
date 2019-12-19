32bba0f843f257165cc1291636b90a050b37ab1b
sayalisheth <sayalicsheth1506@gmail.com>
2019-12-19 12:58:27 +0530

    hangman using methods

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,10 @@
-def find_ind(word,let)
-	i = 0
+chance=5
+stage=0
+words=["JOSH","SOFTWARE","PIZZA","PASTA","LAPTOP","MOBILE","SCHOOL","BOTTEL","BUS","CAT","QUESTION","PROGRAMMER","KING"]
+
+
+def find_index(word,let)
+   	i = 0
 	all = []
 	while(i!=word.length)
 		if word[i]==let
@@ -13,60 +18,70 @@ def find_ind(word,let)
 end
 
 
+def check_word(curr_word,chance)
+	#puts new_word
+	new_word=""
+	letters_enter=""
 
-
-words=["JOSH","SOFTWARE","PIZZA","PASTA","LAPTOP","MOBILE","SCHOOL","BOTTEL","BUS","CAT","QUEASTION","PROGRAMMER","KING"]
-curr_word = words[rand(words.length)]
-#puts curr_word
-count=5
-new_word=""
-letters_enter=""
-for i in 1..curr_word.length do
-	new_word=new_word+"_"
-end
-
-#puts new_word
-while (new_word!=curr_word and count!=0) do
-	puts "You have #{count} chance"
-	for i in 0..new_word.length do 
-		print " #{new_word[i]}" 
+	for i in 1..curr_word.length do
+		new_word=new_word+"_"
 	end
-			puts"\n\n"
-	puts "Enter any latter"
-	l=gets.chomp.upcase
-	puts l
-	if letters_enter.include? l
-		puts "letter already enter please enter new letter\n\n"
-	else
-		letters_enter+=l
-	
-		if curr_word.include? l
 
-			index_all=find_ind(curr_word,l)
-			index_all.each do|a| 
-				new_word[a]=l
-			end
-			
+
+	while (new_word!=curr_word and chance!=0) do
+		puts "You have #{chance} chance"
+		for i in 0..new_word.length do 
+			print " #{new_word[i]}" 
+		end
+		puts"\n\n"
+		puts "Enter any latter"
+		l=gets.chomp.upcase
+		puts l
+
+		if letters_enter.include? l
+			puts "letter already enter please enter new letter\n\n"
 		else
-			count-=1
-		end
-	end
-	
-	if(count==0)
-		for i in 0..new_word.length do 
-			print " #{new_word[i]}" 
-		end
-		puts "\n\n****YOU LOOS!!!****\n\nword is :#{curr_word}\n\n"
-
-	end
-	
-	if(new_word==curr_word)
-		for i in 0..new_word.length do 
-			print " #{new_word[i]}" 
-		end
-		puts "\n\n****YOU WON!!!****\n\n"
-	end
-end
+			letters_enter+=l
 		
+			if curr_word.include? l
+
+				index_all=find_index(curr_word,l)
+				index_all.each do|a| 
+					new_word[a]=l
+				end
+				
+			else
+				chance-=1
+			end
+		end
+
+		if(chance==0)
+			for i in 0..new_word.length do 
+				print " #{new_word[i]}" 
+			end
+			puts "\n\n****YOU LOOS!!!****\n\nword is :#{curr_word}\n\n"
+			exit
+		end
+		
+		if(new_word==curr_word)
+			for i in 0..new_word.length do 
+				print " #{new_word[i]}" 
+			end
+			puts "\n\n****You Guess Correct!!!****\n\n"
+		end
+		end
+		chance
+end
+
+
+
+while(stage<5)
+	stage+=1
+	puts "stage : #{stage}"
+
+	curr_word = words[rand(words.length)]
+	
+	chance=check_word(curr_word,chance)
+end		
 
 

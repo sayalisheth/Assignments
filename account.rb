fe179c32f51a359c09e0b34e90d6a260f83cd66f
sayalisheth <sayalicsheth1506@gmail.com>
2019-12-24 17:05:10 +0530

    updated bank account system

--- account.rb
+++ account.rb
@@ -1,135 +1,141 @@
+
 class Account
- 
-  attr_reader :acc_no, :name, :balance, :type
-	@@first_account_no = 100
+  
+  attr_accessor :first_account_no, :min_balance, :acc_no, :name, :balance, :type
+  
+	@@first_account_no = 99
   @@min_balance = 1000
+
   def initialize(name, amount, type)
-   	 @@first_account_no += 1
+     @@first_account_no += 1
      @name = name
      @acc_no = @@first_account_no
      @balance = amount
      @type = type
   end
 
-  def self.deposit(account_no, amount)
-  	account_no[1] += amount
-    puts "Amount is deposited"
+  def calculate_amount(account, months, rate_of_interest)
+  amount = account.balance * rate_of_interest * months / 1200
   end
 
-  def self.withdraw(account_no, amount)
-  	if account_no[1] - amount < @@min_balance
-  		puts "You can not withdraw"
+  def deposit(account, amount)
+  	account.balance += amount 
+  end
+
+  def withdraw(account, amount)
+  	if account.balance - amount < @@min_balance
+      return 0
     else
-    	account_no[1] -= amount
-    	puts "Amount is withdrawl"
+    	account.balance -= amount
+      return 1
     end
   end
 
-  def self.get_balance(account_no)
-  	puts "\nCustomer name: #{account_no[0]}\nType of account : #{account_no[2]}\nYour current balance is #{account_no[1]}\n\n"
+  def get_balance(account)
+  	puts "\nCustomer name: #{account.name}\nAccount no : #{account.acc_no}\nType of account : #{account.type}\nYour current balance is #{account.balance}\n\n"
   end
 
-  def self.calculate_amount(account_no, months)
-    if account_no[2] == "saving"
-      SavingAccount.calculate_amount(account_no, months)
-    else
-      CurrentAccount.calculate_amount(account_no, months)
-    end
-  end
-
-  def self.create_account(name, type, amount)
-  account_info = []
+  def create_account(name, type, amount)
     if amount < @@min_balance
       puts "Please enter amount greater than min_balance"
     else
-      if type == "saving"
+      if type == ACCOUNT_TYPE_SAVING
         new_account = SavingAccount.new(name, amount)
-
-      elsif type == "current"
+      elsif type == ACCOUNT_TYPE_CURRENT
         new_account = CurrentAccount.new(name, amount)
       else
         puts "Not valid account type"
       end
-      if type == "saving" or type == "current"
-        account_info << new_account.name
-        account_info << new_account.balance
-        account_info << new_account.type
-        #puts account_info
-        Accounts_all[new_account.acc_no] = account_info
+      if type == ACCOUNT_TYPE_SAVING or type == ACCOUNT_TYPE_CURRENT
+        Accounts[@@first_account_no] = new_account
+        puts Accounts
       end
     end
-  
-end
-
-
+  end
 end  
 
 class SavingAccount < Account
 	attr_accessor :ROI
   @@ROI = 9.75
+
 	def initialize(name, amount)
-    super(name, amount, "saving")
+    super(name, amount, ACCOUNT_TYPE_SAVING)
     puts "----Your saving account is created----"
 	end	
 
-	def self.calculate_amount(account_no, months)
-    amt = account_no[1] * @@ROI * months / 100 
-    puts "You will get interest of Rs. #{amt} on balance of Rs. #{account_no[1]} for period of #{months} months with ROI #{@@ROI}"
+	def calculate_amount(account, months)
+    #amount = super.calculate_amount(account, months, @@ROI)
+    amount = account.balance * @@ROI * months / 1200
+    puts "You will get interest of Rs. #{amount} on balance of Rs. #{account.balance} for period of #{months} months with ROI #{@@ROI}"
 	end
 end
 
 class CurrentAccount < Account
   attr_accessor :ROI
   @@ROI = 7.25
+
   def initialize(name, amount)
-    super(name, amount, "current")
+    super(name, amount, ACCOUNT_TYPE_CURRENT)
     puts "----Your current account is created----"
   end 
 
-  def self.calculate_amount(account_no, months)
-    amt = account_no[1] * @@ROI * months / 1200
-    puts "You will get interest of Rs. #{amt} on balance of Rs. #{@balance} for period of #{months} months with ROI #{@@ROI}"
+  def calculate_amount(account, months)
+    #amount = super.calculate_amount(account, months, @@ROI)
+    amount = account.balance * @@ROI * months / 1200
+    puts "You will get interest of Rs. #{amount} on balance of Rs. #{account.balance} for period of #{months} months with ROI #{@@ROI}"
   end
 end
 
-Accounts_all = {}
+Accounts = Hash.new 
+ACCOUNT_TYPE_SAVING = "saving"
+ACCOUNT_TYPE_CURRENT = "current"
+
 while 1
-  puts "Enter your choice\n1.Create account\n2.Withdraw\n3.Deposit\n4.Check balance\n5.check ROI\n6.Exit"
+  puts "______________________\nEnter your choice\n1.Create account\n2.Withdraw\n3.Deposit\n4.Check balance\n5.check ROI\n6.Exit"
   choice = gets.chomp.to_i
   if choice != 1 and choice <= 5
     puts "Enter account no"
     acc_no = gets.chomp.to_i
+    if !Accounts.has_key? acc_no
+      puts "Account number does not exist"
+      flag = 0
+    else
+      flag = 1
+    end
   end
-  case choice
-  when 1 
-    puts "Enter name of customer"
-    name = gets.chomp
-    puts "Enter account type(saving or current)"
-    type = gets.chomp.downcase
-    puts "Enter amount you want to depoist in your account first"
-    amount = gets.chomp.to_f
-    Account.create_account(name, type, amount)
-    puts Accounts_all
-  when 2
-    puts "Enter amount you want to withdraw"
-    amount = gets.chomp.to_f
-    Account.withdraw(Accounts_all[acc_no], amount) 
-  when 3
-    puts "Enter amount you want to deposite"
-    amount = gets.chomp.to_f
-    Account.deposit(Accounts_all[acc_no], amount) 
-  when 4
-   Account.get_balance(Accounts_all[acc_no]) 
-  when 5
-    puts "Enter no of month "
-    months = gets.chomp.to_i
-    Account.calculate_amount(Accounts_all[acc_no], months) 
-  when 6
-    exit
+
+  if flag == 1 or choice == 6 or choice == 1
+    case choice
+    when 1 
+      puts "Enter name of customer"
+      name = gets.chomp
+      puts "Enter account type(saving or current)"
+      type = gets.chomp.downcase
+      puts "Enter amount you want to depoist in your account first"
+      amount = gets.chomp.to_f
+      Account.new("first",0,ACCOUNT_TYPE_SAVING).create_account(name, type, amount)
+    when 2
+      puts "Enter amount you want to withdraw"
+      amount = gets.chomp.to_f
+      status=Accounts[acc_no].withdraw(Accounts[acc_no], amount) 
+      if status == 1
+        puts "Amount is withdrawl"
+      else
+        puts "You can not withdraw"
+      end
+    when 3
+      puts "Enter amount you want to deposit"
+      amount = gets.chomp.to_f
+      Accounts[acc_no].deposit(Accounts[acc_no], amount) 
+      puts "Amount is deposited"
+    when 4
+     Accounts[acc_no].get_balance(Accounts[acc_no]) 
+    when 5
+      puts "Enter no of month "
+      months = gets.chomp.to_i
+      Accounts[acc_no].calculate_amount(Accounts[acc_no], months) 
+    when 6
+      exit
+    end
   end
 end
-
-
-
-
-

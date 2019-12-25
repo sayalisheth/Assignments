8b2c8e5aef1544c61d7c680111c272d2aab3ecf3
sayalisheth <sayalicsheth1506@gmail.com>
2019-12-25 11:21:13 +0530

    updated foodorder system

--- FoodOrder.rb
+++ FoodOrder.rb
@@ -1,67 +1,75 @@
 class FoodOrder
-	@@Menu = ["ROTI", "RICE", "PANJABI THALI", "GUJARATHI THALI", "DOSA", "PIZZA", "PASTA"]
-	
-	def initialize(order, name)
-		@name=name
-    @order = order
-    confirm
+	@@Menu = {"ROTI"=>25, "RICE"=>100, "PANJABI THALI"=>150, "GUJARATHI THALI"=>160, "DOSA"=>100, "PIZZA"=>250, "PASTA"=>200}
+	@@food_orders = {}
+	def initialize(name, orders)
+		@@food_orders[name]=orders
+    confirm_food_oder
+  end
+
+  def self.accept_food_oder
+  	puts "Enter customer name"
+	  name = gets.chomp
+		order_list = {}
+		bill = 0
+			while 1
+				FoodOrder.show_menu
+				puts "\nPress any key to continue\nPress n to stop\n\n"
+				key = gets.chomp
+				if key != 'n'
+					puts "Enter your order"
+					order = gets.chomp.upcase
+					if @@Menu.has_key?order
+						puts order
+						order_list[order] = @@Menu[order]
+						puts order_list
+					else 
+						puts "Item is not avaliable"
+					end	
+				else
+					order_list.each do |order|
+						bill += order[1]
+					end
+					if bill == 0
+						puts "You do not order anything"
+						break
+					else
+						puts "Your order is:"
+						puts order_list
+						puts "---Your order is accepted----\n You have to pay Rs #{bill}"
+						customer = FoodOrder.new(name, order_list)
+						break
+					end
+				end
+			end
+	  
   end
 
 	def self.show_menu
 		puts "-----Menu-----"
-			for i in 0..@@Menu.length do
-				puts @@Menu[i]
-			end
+				puts @@Menu
 	end
 
-	def confirm
-		flag = 1
-		for i in 0..@order.length-1 do
-		
-			if (!@@Menu.include? @order[i])
-				puts "item is not avaliable now"
-				flag = 0
-				break;
-			end
-		end
-		if flag == 1
-			sleep(3)
-			puts "----Your order is confirmed----"
-			deliver
-		else
-			sleep(3)
-			puts "----Your order is not confirmed----"
-		end
+	def confirm_food_oder
+		sleep(3)
+		puts "----Your order is not confirmed----"
+		deliver_food_oder
 	end
 
-	def deliver
+	def deliver_food_oder
 		sleep(5)
 		puts "----Your order is deliver----"
+		puts "\n#{@@food_orders}\n"
 	end
-
 end
 
-
-for i in 0..1 do
- puts "Enter customer name"
- name = gets.chomp
-	order_list = []
-	while(1)
-		FoodOrder.show_menu
-		puts "Enter your order"
-		order = gets.chomp.upcase
-		order_list.push(order)
-			
-		puts "\nPress any key to continue\nPress n to stop\n\n"
-		key = gets.chomp
-		if key == "n"
-			puts "Your order is:"
-			for i in 0..order_list.length do
-				puts order_list[i]
-			end
-			puts "---Your order is accepted----"
-			break;
-		end
+while 1
+	puts "-------------------------\nYou want to order food?\nIf YES press y or if NO press n"
+	choice = gets.chomp.upcase
+	if choice == "Y"
+		FoodOrder.accept_food_oder
+	elsif choice == "N"
+		exit
+	else
+		puts "Invalid choice"
 	end
-	customer = FoodOrder.new(order_list,name)
-end	
+end
